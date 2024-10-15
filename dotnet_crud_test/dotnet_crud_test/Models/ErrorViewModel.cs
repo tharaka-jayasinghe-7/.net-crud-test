866ea5b2366691f6334f174171c38a7e006bc923
Tharaka Jayasinghe <124444782+tharaka-jayasinghe-7@users.noreply.github.com>
2024-10-15 23:09:46 +0530

    db config

---
+++ dotnet_crud_test/dotnet_crud_test/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dotnet_crud_test.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
